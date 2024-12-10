a7af62bc98654d2c78f03b9d562e05c326f686f0
dmitryyyan <dima2003yan@gmail.com>
2024-12-11 02:46:12 +0300

    s

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig(({ command }) => {
     build: {
       sourcemap: true,
       rollupOptions: {
-        input: './src/index.html',
+        input: 'index.html',
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
@@ -40,9 +40,7 @@ export default defineConfig(({ command }) => {
     plugins: [
       injectHTML(),
       FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
+      
     ],
   };
 });
