5d107fdbb216746905d1fb7c699978ceb80d7239
dmitryyyan <dima2003yan@gmail.com>
2024-12-11 03:04:45 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
+import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
@@ -12,35 +11,20 @@ export default defineConfig(({ command }) => {
     root: 'src',
     build: {
       sourcemap: true,
+
       rollupOptions: {
-        input: 'index.html',
+        input: glob.sync('./src/*.html'),
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
               return 'vendor';
             }
           },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
+          entryFileNames: 'commonHelpers.js',
         },
       },
       outDir: '../dist',
-      emptyOutDir: true,
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
-      
-    ],
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
   };
 });
